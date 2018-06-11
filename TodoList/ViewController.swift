58f270106dff5d5a587462d71e7b875f5fbb1ea8
sizukutamago <tanaka@tam-bourine.co.jp>
2018-06-11 15:35:55 +0900

    todo

--- TodoList/ViewController.swift
+++ TodoList/ViewController.swift
@@ -8,8 +8,26 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource
+ {
 
+    @IBOutlet weak var TableView: UITableView!
+    
+    let todos = ["トイレ", "洗濯", "出社", "帰る", "寝る"]
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return todos.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell: UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "Todo", for: indexPath)
+        
+        // セルに表示する値を設定する
+        cell.textLabel!.text = todos[indexPath.row]
+        
+        return cell
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
